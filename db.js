d98c0cacdbcc2a2ee640e7897fcc13f3062eb61c
Fikkii <98499137+Fikkii@users.noreply.github.com>
2023-07-24 15:34:38 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Fikkyart:<password>@cluster0.2iaq2am.mongodb.net/', {
+mongoose.connect('mongodb+srv://Fikkyart:mydadaskedmetoprepare@cluster0.2iaq2am.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('conected to database successfully'))
